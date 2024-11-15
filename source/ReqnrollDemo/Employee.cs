cf1572c5d86a229b537558e66f16f41a10d7c38d
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-11-15 10:33:41 +0100

    feat: added classes to do dummy tests on, for the demo project.

---
+++ source/ReqnrollDemo/Employee.cs
@@ -0,0 +1,15 @@
+namespace ReqnrollDemo;
+
+public class Employee(string name, Role role)
+{
+    public Guid Id { get;} = Guid.NewGuid();
+    public string Name { get;} = name;
+    public Role Role { get; set; } = role;
+}
+
+public enum Role
+{
+    Manager,
+    Engineer,
+    Contractor
+}

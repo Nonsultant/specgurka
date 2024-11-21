1654a8e46f624df2ab88974a0b1d7aea7caac318
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-11-21 15:50:10 +0100

    feat: extension method on the TestProject.cs that will set configuration based on arguments.

---
+++ source/GenGurka/Extensions/TestProjectExtensions.cs
@@ -0,0 +1,35 @@
+namespace SpecGurka.GenGurka.Extensions;
+
+internal static class TestProjectExtensions
+{
+    internal static void ApplyArgumentConfiguration(this TestProject testProject, Dictionary<string, string> arguments)
+    {
+        foreach (var argument in arguments)
+        {
+            switch (argument.Key)
+            {
+                case "-trx":
+                    testProject.TestResultFile = argument.Value;
+                    break;
+
+                case "-o":
+                case "--output-path":
+                    testProject.OutputPath = argument.Value;
+                    break;
+
+                case "-f":
+                case "--feature-directory":
+                    testProject.FeaturesDirectory = argument.Value;
+                    break;
+
+                case "-a":
+                case "--assembly":
+                    testProject.AssemblyFile = argument.Value;
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unknown argument: {argument.Key}");
+            }
+        }
+    }
+}

23577159f75b83793f2dac4bb5387cdde60e938a
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-11-29 13:09:49 +0100

    feat: moved the showing of features to the dynamic feature page.
    Refs: #7

--- source/VizGurka/Pages/Features/Index.cshtml.cs
+++ source/VizGurka/Pages/Features/Index.cshtml.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpecGurka.GurkaSpec;
 
 namespace VizGurka.Pages.Features;
 
 public class Index : PageModel
 {
-    public Guid Id { get; set; }
+    public Feature Feature { get; set; }
 
     public void OnGet(Guid id)
     {
-        Id = id;
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GurkaFiles/DemoProject_2024-11-28T13_21_37.gurka");
+
+        var file = Gurka.ReadGurkaFile(filePath).Products.FirstOrDefault();
+
+        Feature = file.Features.FirstOrDefault(f => f.Id == id);
     }
 }
--- source/VizGurka/Pages/Index.cshtml.cs
+++ source/VizGurka/Pages/Index.cshtml.cs
@@ -1,21 +1,13 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SpecGurka.GurkaSpec;
 
 namespace VizGurka.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly string? _gurkaFilePath;
-    public Testrun TestRun { get; set; }
-
-    public IndexModel(IConfiguration config)
-    {
-        _gurkaFilePath = config.GetValue<string>("GurkaFilePath");
-    }
+    public IndexModel() { }
 
     public void OnGet()
     {
         ViewData["Title"] = "Gurka";
-        TestRun = Gurka.ReadGurkaFile(_gurkaFilePath);
     }
 }

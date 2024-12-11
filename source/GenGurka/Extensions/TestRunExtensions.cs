b73bc98d3b3bf3f1a2d745d579337ea10bc57e71
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-12-11 14:31:27 +0100

    refactor: refactored this together with the mapping between gherkindoc and features.

--- source/GenGurka/Extensions/TestRunExtensions.cs
+++ source/GenGurka/Extensions/TestRunExtensions.cs
@@ -7,35 +7,17 @@ internal static class TestRunExtensions
 {
     internal static void MatchWithGurkaFeatures(this TestRun testRun, Product gurkaProject)
     {
-            foreach (var feature in gurkaProject.Features)
+        foreach (var feature in gurkaProject.Features)
+        {
+            foreach (var utr in testRun.Results.UnitTestResults)
             {
-                if (feature.Status == Status.NotImplemented)
-                {
-                    feature.Scenarios.ForEach(scenario =>
-                    {
-                        scenario.Status = Status.NotImplemented;
-                        scenario.Steps.ForEach(step => step.Status = Status.NotImplemented);
-                    });
-                    continue;
-                }
+                var sceUnderTest = feature.GetScenario(utr.TestName);
 
-                foreach (var utr in testRun.Results.UnitTestResults)
-                {
-                    var sceUnderTest = feature.GetScenario(utr.TestName);
+                if (sceUnderTest is null || utr.Outcome == "NotExecuted") continue;
 
-                    if (sceUnderTest is null) continue;
-
-                    if (utr.Outcome == "NotExecuted")
-                    {
-                        sceUnderTest.Status = Status.NotImplemented;
-                        sceUnderTest.Steps.ForEach(step => step.Status = Status.NotImplemented);
-                        continue;
-                    }
-
-                    sceUnderTest.Status = utr.Outcome == "Passed" ? Status.Passed : Status.Failed;
-                    sceUnderTest.TestDuration = utr.Duration;
-                    feature.ParseTestOutput(utr.Output.StdOut);
-                }
+                sceUnderTest.TestDuration = utr.Duration;
+                feature.ParseTestOutput(utr.Output.StdOut, sceUnderTest);
             }
+        }
     }
 }

476d0993c5474929c7d666c768218e1e0d6d7357
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-12-17 11:00:37 +0100

    refactor: changed small things in alot of files, changed some styling faults.

---
+++ source/VizGurka/Helpers/DurationHelper.cs
@@ -0,0 +1,19 @@
+namespace VizGurka.Helpers
+{
+    public static class DurationHelper
+    {
+        public static string FormatDuration(string stringDuration)
+        {
+            TimeSpan duration = TimeSpan.Parse(stringDuration);
+
+            if (duration.TotalMinutes >= 1)
+            {
+                return $"{(int)duration.TotalMinutes}m {duration.Seconds}s";
+            }
+            else
+            {
+                return $"{duration.Seconds}s";
+            }
+        }
+    }
+}

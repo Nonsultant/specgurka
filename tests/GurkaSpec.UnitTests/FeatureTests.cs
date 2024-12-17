be538a26bca40e4c82e26c839adbacca7ef5ca2e
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-12-17 09:04:07 +0100

    refactor: rewrote failing tests.

--- tests/GurkaSpec.UnitTests/FeatureTests.cs
+++ tests/GurkaSpec.UnitTests/FeatureTests.cs
@@ -1,18 +1,29 @@
 namespace GurkaSpec.UnitTests;
 
-public class ScenarioTests
+public class FeatureTests
 {
+    private readonly Feature _feature;
 
-    private readonly Scenario _scenario = new Scenario()
+    public FeatureTests()
     {
-        Name = "Some scenario",
-        Steps =
-        [
-            new Step { Kind = "Given", Text = "some precondition" },
-            new Step { Kind = "When", Text = "some action is performed" },
-            new Step { Kind = "Then", Text = "some result is expected" }
-        ]
-    };
+        _feature = new Feature()
+        {
+            Name = "Some feature",
+            Scenarios =
+            [
+                new Scenario
+                {
+                    Name = "Some scenario",
+                    Steps =
+                    [
+                        new Step { Kind = "Given", Text = "some precondition" },
+                        new Step { Kind = "When", Text = "some action is performed" },
+                        new Step { Kind = "Then", Text = "some result is expected" }
+                    ]
+                }
+            ]
+        };
+    }
 
     [Fact]
     public void ParseTestOutput_GivenAPassingScenario_ShouldParseCorrectly()
@@ -26,10 +37,10 @@ public class ScenarioTests
                         "-> done: Tests.ThenSomeResultIsExpected (0.789s)";
 
         // Act
-        _scenario.ParseTestOutput(output);
+        _feature.ParseTestOutput(output, _feature.Scenarios[0]);
 
         // Assert
-        _scenario.Steps.ForEach(step => step.TestPassed.Should().BeTrue());
+        _feature.Scenarios[0].Steps.ForEach(step => step.Status.Should().Be(Status.Passed));
     }
 
     [Fact]
@@ -44,17 +55,16 @@ public class ScenarioTests
                         "-> error: Tests.ThenSomeResultIsExpected Failure: Strings differ\n" +
                         "Expected: \"Expected\"\n" +
                         "Actual: \"Not-Expected\"\n" +
-                        "\u2191 (pos 11) (0.0s)<";
+                        "\u2191 (pos 11) (0.0s)";
 
         // Act
-        _scenario.ParseTestOutput(output);
+        _feature.ParseTestOutput(output, _feature.Scenarios[0]);
 
         // Assert
-        _scenario.TestPassed.Should().BeFalse();
-
-        _scenario.Steps[0].TestPassed.Should().BeTrue();
-        _scenario.Steps[1].TestPassed.Should().BeTrue();
-        _scenario.Steps[2].TestPassed.Should().BeFalse();
+        _feature.Status.Should().Be(Status.Failed);
+        _feature.Scenarios[0].Status.Should().Be(Status.Failed);
+        _feature.Scenarios[0].Steps[0].Status.Should().Be(Status.Passed);
+        _feature.Scenarios[0].Steps[1].Status.Should().Be(Status.Passed);
+        _feature.Scenarios[0].Steps[2].Status.Should().Be(Status.Failed);
     }
-
 }

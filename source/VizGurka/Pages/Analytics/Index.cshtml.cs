dc3104ae96a8a24e53ad326e9b4d5049922f5001
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-12-18 11:29:39 +0100

    feat: analytics page with table that shows the slowest scenarios.

---
+++ source/VizGurka/Pages/Analytics/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpecGurka.GurkaSpec;
+using VizGurka.Helpers;
+
+namespace VizGurka.Pages.Analytics;
+
+public class Index : PageModel
+{
+    public List<(Guid FeatureId, Scenario Scenario)> SlowestScenarios { get; set; }
+
+    public void OnGet()
+    {
+        var testRun = TestrunReader.ReadLatestRun();
+        var product = testRun.Products.FirstOrDefault();
+
+        SlowestScenarios = product.Features
+            .SelectMany(f => f.Scenarios.Select(s => (f.Id, s)))
+            .OrderByDescending(s => s.s.TestDuration)
+            .Take(5)
+            .ToList();
+    }
+}

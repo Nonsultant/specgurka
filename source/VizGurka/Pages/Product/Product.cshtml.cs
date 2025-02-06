6526c8b628ae7bc03d064c5f148cc35267a24dd7
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-06 10:29:14 +0100

    working on importing data correct and dynamically

--- source/VizGurka/Pages/Product/Product.cshtml.cs
+++ source/VizGurka/Pages/Product/Product.cshtml.cs
@@ -1,20 +1,35 @@
+using Markdig;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpecGurka.GurkaSpec;
 using VizGurka.Helpers;
-
 namespace VizGurka.Pages.Product;
 
 public class ProductModel : PageModel
 {
+    public Feature Feature { get; set; }
+    public MarkdownPipeline Pipeline { get; set; }
     public string ProductName { get; set; }
     public DateTime LatestRunDate { get; set; }
 
-    public void OnGet(string productName)
+    public void OnGet(string productName, Guid id)
     {
         ProductName = productName;
         var latestRun = TestrunReader.ReadLatestRun(productName);
+        var product = latestRun.Products.FirstOrDefault();
+        Feature = product.Features.FirstOrDefault(f => f.Id == id);
+
         if (latestRun != null)
         {
             LatestRunDate = DateTime.Parse(latestRun.DateAndTime);
         }
+
+        Pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+    }
+    public IHtmlContent MarkdownStringToHtml(string input)
+    {
+        var trimmedInput = input.Trim();
+
+        return new HtmlString(Markdown.ToHtml(trimmedInput, Pipeline));
     }
 }

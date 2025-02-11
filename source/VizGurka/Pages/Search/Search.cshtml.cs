40ac1ecaac43dd0ae35f7b970a1d89064538f26a
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-11 15:58:02 +0100

    Now possible to search for feature names

--- source/VizGurka/Pages/Search/Search.cshtml.cs
+++ source/VizGurka/Pages/Search/Search.cshtml.cs
@@ -10,21 +10,31 @@ namespace VizGurka.Pages.Search
     public class SearchModel : PageModel
     {
         private readonly IStringLocalizer<SearchModel> _localizer;
-
+        public List<Feature> Features { get; set; } = new List<Feature>();
+        public List<Scenario> Scenarios { get; set; } = new List<Scenario>();
         public string ProductName { get; set; }
         public string Query { get; set; }
         public List<Feature> SearchResults { get; set; } = new List<Feature>();
+        public DateTime LatestRunDate { get; set; }
 
         public void OnGet(string productName, string query)
         {
             ProductName = productName;
             Query = query;
+            var latestRun = TestrunReader.ReadLatestRun(productName);
+            var product = latestRun?.Products.FirstOrDefault();
+            if (product != null)
+            {
+                PopulateFeatures(product);
+            }
+
+            if (latestRun != null)
+            {
+                LatestRunDate = DateTime.Parse(latestRun.DateAndTime);
+            }
 
             if (!string.IsNullOrEmpty(Query))
             {
-                // Perform the search logic here
-                var latestRun = TestrunReader.ReadLatestRun(productName);
-                var product = latestRun?.Products.FirstOrDefault();
                 if (product != null)
                 {
                     SearchResults = product.Features
@@ -32,6 +42,19 @@ namespace VizGurka.Pages.Search
                         .ToList();
                 }
             }
+        }
+
+        private void PopulateFeatures(SpecGurka.GurkaSpec.Product product)
+        {
+            Features = product.Features.Select(f => new Feature
+            {
+                Id = f.Id,
+                Name = f.Name,
+                Status = f.Status,
+                Scenarios = f.Scenarios,
+                Rules = f.Rules,
+                Description = f.Description
+            }).ToList();
         }
     }
 }

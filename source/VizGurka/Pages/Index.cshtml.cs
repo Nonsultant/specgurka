760c8c78aacbce9339780ff5137fc54420c12754
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-11 14:17:38 +0100

    Add localization support to Index page and log current culture

--- source/VizGurka/Pages/Index.cshtml.cs
+++ source/VizGurka/Pages/Index.cshtml.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VizGurka.Helpers;
-
+using Microsoft.Extensions.Localization;
+using System.Globalization;
 
 namespace VizGurka.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly IStringLocalizer<IndexModel> _localizer;
+    public IndexModel(IStringLocalizer<IndexModel> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public string title_box_title { get; set; }
+    public string test_choice_box_title { get; set; }
     public List<(string ProductName, DateTime LatestRunDate, Guid Id)> UniqueProductNamesWithDatesAndId { get; set; } = new List<(string ProductName, DateTime LatestRunDate, Guid Id)>();
 
     public void OnGet()
     {
+        // this is just for logging purposes
+        var currentCulture = CultureInfo.CurrentCulture.Name;
+        Console.WriteLine($"Current Culture: {currentCulture}");
+
+        title_box_title = _localizer["title_box_title"];
+        test_choice_box_title = _localizer["test_choice_box_title"];
+
         var uniqueProductNames = TestrunReader.GetUniqueProductNames();
 
         foreach (var productName in uniqueProductNames)
@@ -30,5 +46,6 @@ public class IndexModel : PageModel
         UniqueProductNamesWithDatesAndId = UniqueProductNamesWithDatesAndId
             .OrderByDescending(item => item.LatestRunDate)
             .ToList();
+
     }
 }

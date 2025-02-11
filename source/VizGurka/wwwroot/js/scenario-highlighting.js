5253ac84294471cb1a08ae18be562a89dac17f27
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-11 14:21:02 +0100

    Refactor CSS styles and remove unused JavaScript files

--- source/VizGurka/wwwroot/js/scenario-highlighting.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.scenario').forEach(scenario => {
-    scenario.addEventListener('click', () => {
-      window.location.hash = scenario.id;
-    });
-  });
-});

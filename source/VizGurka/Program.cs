a77bf2c6a1e2384924e63b39e864fa138102fbe7
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-02-19 09:42:27 +0100

    feat: Localization support (#26)
    * Refactor the Feature display page
    
    * Start of a redesign of analytics
    
    * refactor featuresLayout
    
    * Removing redundant layouts and add indexlayout
    
    * Replace _IndexLayout with a new _Layout file for improved structure
    
    * Refactor layout structure
    
    * Update layout reference and bind model properties in Index page
    
    * Add localization support to Index page and log current culture
    
    * Add Swedish and British English localization support for Index page
    
    * Refactor CSS styles and remove unused JavaScript files
    
    * Add localization support with English and Swedish cultures in Program.cs
    
    * Remove Product page and update layout reference in Index page
    
    * Remove Redundant Visual Studio solution file
    
    * Moved navbar out of layout. Now using @Localizer instead of creating localization models in the backcode. DateTime is now using InvariantCulture to ensure proper Globalization.
    
    * Changed the Localization so that english is the default language.

--- source/VizGurka/Program.cs
+++ source/VizGurka/Program.cs
@@ -7,6 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddRazorPages()
+    .AddViewLocalization();
+
+var supportedCultures = new[] { "en-GB", "sv-SE" };
+var defaultCulture = "en-GB";
+
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(defaultCulture)
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,14 +30,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// The order of these middleware is important for localization to work as intended
+app.UseRequestLocalization(localizationOptions);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
 
 app.Run();
-

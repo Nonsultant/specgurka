99c3d866caddfa6bc77b8f893dbb313d8ab50d5a
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-04-02 10:46:40 +0200

    Add TagViewModel and razor partial for the tag link handling.

---
+++ source/VizGurka/Models/TagViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace VizGurka.Models
+{
+    public class TagViewModel
+    {
+        public string Tag { get; set; }
+        public string GithubBaseUrl { get; set; }
+        public string GithubOwner { get; set; }
+        public string GithubRepoName { get; set; } 
+        public string AzureBaseUrl { get; set; }
+        public string AzureRepoName { get; set; }
+    }
+}

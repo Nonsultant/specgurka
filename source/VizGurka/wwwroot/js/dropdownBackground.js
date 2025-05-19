d9a834ee6659f93173414967a23026e5dabb2668
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-05-19 08:47:41 +0200

    add a backgorund dropdown to each scenario card

---
+++ source/VizGurka/wwwroot/js/dropdownBackground.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.dropdown-toggle-background').forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            var targetId = btn.getAttribute('data-target');
+            var content = document.getElementById(targetId);
+            if (content.style.display === "none" || content.style.display === "") {
+                content.style.display = "block";
+            } else {
+                content.style.display = "none";
+            }
+        });
+    });
+});

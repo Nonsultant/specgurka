c28ff84fb397d04be5f455b629b0eb9fd8ded5d5
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-05-20 10:14:39 +0200

    moved GitHubFetcher.cs to Vizgurka/models. Consolidated the SearchResult.cs in GurkaSpec with the SearchResult.cs in VizGurka/Models

--- source/VizGurka/Models/SearchResult.cs
+++ source/VizGurka/Models/SearchResult.cs
@@ -1,24 +1,33 @@
-﻿namespace VizGurka.Models
+﻿namespace VizGurka.Models;
+
+public class SearchResult
 {
-    public class SearchResult
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
-        public float Score { get; set; }
-        public string SourceField { get; set; } = string.Empty;
-        public string DocumentType { get; set; } = string.Empty;
-        public string StepText { get; set; } = string.Empty;
+    public string DocType { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public float Score { get; set; }
+    public List<string> Tags { get; set; } = new List<string>();
+    public List<string> TypeSpecificTags { get; set; } = new List<string>();
+        
+    public string Id { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+        
+    public string ParentFeatureId { get; set; } = string.Empty;
+    public string ParentFeatureName { get; set; } = string.Empty;
+    public string Duration { get; set; } = string.Empty;
+        
+    public string BranchName { get; set; } = string.Empty;
+    public string CommitId { get; set; } = string.Empty;
+    public string CommitMessage { get; set; } = string.Empty;
+    public string TestsPassed { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
 
-        public List<string> Tags { get; set; } = new();
-        public List<string> TypeSpecificTags { get; set; } = new();
+    public string SourceField { get; set; } = string.Empty;
+    public string DocumentType { get; set; } = string.Empty;
+    
+    public string StepText { get; set; } = string.Empty;
 
-        public string FeatureId { get; set; } = string.Empty;
-        public string Status { get; set; } = string.Empty;
-
-        public string ParentFeatureId { get; set; } = string.Empty;
-        public string ParentFeatureName { get; set; } = string.Empty;
-        public string Duration { get; set; } = string.Empty;
-
-        public string FileName { get; set; } = string.Empty;
-    }
+    public string FeatureId { get; set; } = string.Empty;
+    
 }

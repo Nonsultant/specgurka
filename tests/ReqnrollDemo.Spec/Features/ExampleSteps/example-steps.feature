50acef2e64f32a239dcabcd17d44f69f78efd4e9
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-05-20 13:25:02 +0200

    Scenario cards now has an additional harmonica for rule background.
    Add localization.
    
    Include the example-steps.feature

---
+++ tests/ReqnrollDemo.Spec/Features/ExampleSteps/example-steps.feature
@@ -0,0 +1,12 @@
+﻿Feature: Example
+
+    Background:
+        Given feature background step
+
+    Rule: Example rule
+
+        Background:
+            Given rule background step
+
+        Scenario: Example scenario
+            Given scenario step
---
+++ tests/ReqnrollDemo.Spec/StepDefinitions/ExampleStepsStepDefinitions.cs
@@ -0,0 +1,54 @@
+﻿namespace ReqnrollDemo.Spec.StepDefinitions;
+
+using Reqnroll;
+using System;
+using System.Collections.Generic;
+
+[Binding]
+public class ExampleStepsStepDefinitions
+{
+    // Will be unique per scenario
+    private readonly ScenarioContext _scenarioContext;
+
+    public ExampleStepsStepDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given(@"feature background step")]
+    public void GivenFeatureBackgroundStep()
+    {
+        AddStep("feature background step");
+    }
+
+    [Given(@"rule background step")]
+    public void GivenRuleBackgroundStep()
+    {
+        AddStep("rule background step");
+    }
+
+    [Given(@"scenario step")]
+    public void GivenScenarioStep()
+    {
+        AddStep("scenario step");
+    }
+
+    private void AddStep(string step)
+    {
+        if (!_scenarioContext.ContainsKey("stepLog"))
+            _scenarioContext["stepLog"] = new List<string>();
+        ((List<string>)_scenarioContext["stepLog"]).Add(step);
+    }
+
+    [AfterScenario]
+    public void AfterScenario()
+    {
+        if (_scenarioContext.TryGetValue("stepLog", out var logObj) && logObj is List<string> log)
+        {
+            Console.WriteLine("=== Step Execution Order ===");
+            foreach (var entry in log)
+                Console.WriteLine(entry);
+            Console.WriteLine("===========================");
+        }
+    }
+}
